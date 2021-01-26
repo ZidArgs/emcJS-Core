c533e34ab62a41c86c6ba2a418d17e34e5c4e9d7
Denis Peters <denis.peters@udo.edu>
2021-01-26 12:46:23 +0100

    remove inject functions from import;

--- src/util/import/Import.js
+++ src/util/import/Import.js
@@ -78,64 +78,6 @@ class Import {
             return await getCSS(url);
         }
     }
-    
-    addStyle(url) {
-        return new Promise((res, rej) => {
-            const t = document.createElement("link");
-            t.rel = "stylesheet";
-            t.type = "text/css";
-            t.onload = function() {
-                res(t);
-            };
-            t.onerror = function() {
-                getFile(url).then(function() {
-                    rej(`error appending style "${url}"`);
-                }, function(r) {
-                    rej(r);
-                })
-            };
-            t.href = url;
-            document.head.append(t);
-        });
-    }
-    
-    addScript(url) {
-        return new Promise((res, rej) => {
-            const t = document.createElement("script");
-            t.type = "text/javascript";
-            t.onload = function() {
-                res(t);
-            };
-            t.onerror = function() {
-                getFile(url).then(function() {
-                    rej(`error appending script "${url}"`);
-                }, function(r) {
-                    rej(r);
-                })
-            };
-            t.src = url;
-            document.head.append(t);
-        });
-    }
-    
-    addModule(url) {
-        return new Promise((res, rej) => {
-            const t = document.createElement("script");
-            t.type = "module";
-            t.onload = function() {
-                res(t);
-            };
-            t.onerror = function() {
-                getFile(url).then(function() {
-                    rej(`error appending module "${url}"`);
-                }, function(r) {
-                    rej(r);
-                })
-            };
-            t.src = url;
-            document.head.append(t);
-        });
-    }
 
 }
 
