df39a67fae55a84d3699b892a102b4c7ffa9df1a
Denis Peters <denis.peters@udo.edu>
2021-02-12 01:06:56 +0100

    add usage comment to i18n input element;

--- src/i18n/ui/InputElement.js
+++ src/i18n/ui/InputElement.js
@@ -1,5 +1,11 @@
 import I18nMixin from "./I18nMixin.js";
 
+/**
+ * usage:
+ * ```html
+ * <input is="emc-i18n-input" type="text" i18nKey="translation.placeholder.key" i18nValue="default placeholder">
+ * ```
+ */
 export default class InputElement extends I18nMixin(HTMLInputElement) {
 
     applyI18nTranslation(content) {
