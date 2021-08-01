6acaeec6b3871df9c746c83b9849fea879c393ae
Denis Peters <denis.peters@udo.edu>
2021-08-01 02:16:34 +0200

    make immutables proxy data recursive;

--- src/data/Immutable.js
+++ src/data/Immutable.js
@@ -7,10 +7,27 @@ const HANDLER = {
     defineProperty() {}
 };
 
+function buildRecursiveProxy(data) {
+    if (typeof data == "object") {
+        if (Array.isArray(data)) {
+            const res = data.map(buildRecursiveProxy);
+            return new Proxy(res, HANDLER);
+        } else {
+            const res = {};
+            for (const key in data) {
+                const value = data[key];
+                res[key] = buildRecursiveProxy(value);
+            }
+            return new Proxy(res, HANDLER);
+        }
+    }
+    return data;
+}
+
 export default class Immutable {
 
     constructor(target) {
-        return new Proxy(target, HANDLER);
+        return buildRecursiveProxy(target);
     }
 
 }
