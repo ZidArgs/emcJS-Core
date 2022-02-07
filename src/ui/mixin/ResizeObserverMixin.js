d6b5a02f3eda1c994d3e8c3c8c1f3a0b36695bb7
Denis Peters <denis.peters@udo.edu>
2022-02-07 18:40:04 +0100

    add resize observer mixin;

---
+++ src/ui/mixin/ResizeObserverMixin.js
@@ -0,0 +1,37 @@
+import {
+    createMixin
+} from "../../util/Mixin.js";
+
+const RESIZE_OBSERVER = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+        const target = entry.target;
+        target.handleResize();
+    }
+});
+
+export default createMixin((superclass) => class ResizeObserverMixin extends superclass {
+
+    connectedCallback() {
+        if (super.connectedCallback) {
+            super.connectedCallback();
+        }
+        RESIZE_OBSERVER.observe(this);
+    }
+
+    disconnectedCallback() {
+        if (super.disconnectedCallback) {
+            super.disconnectedCallback();
+        }
+        RESIZE_OBSERVER.unobserve(this);
+    }
+
+    handleResize() {
+        const ev = new Event("resize");
+        ev.outerWidth = this.offsetWidth;
+        ev.outerHeight = this.offsetHeight;
+        ev.innerWidth = this.clientWidth;
+        ev.innerHeight = this.clientHeight;
+        this.dispatchEvent(ev);
+    }
+
+});
