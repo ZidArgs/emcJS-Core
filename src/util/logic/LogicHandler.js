21d1221e1a307afc7ccd1d3ffc1a7daf716bff52
Denis Peters <denis.peters@udo.edu>
2022-05-16 10:54:52 +0200

    pass special data LogicHandler;

--- src/util/logic/LogicHandler.js
+++ src/util/logic/LogicHandler.js
@@ -1,4 +1,10 @@
+import {
+    debounce
+} from "../Debouncer.js";
+import EventTargetManager from "../event/EventTargetManager.js";
 import LogicCompiler from "./LogicCompiler.js";
+
+const EVENTS = ["load", "clear", "change"];
 
 export default class LogicHandler extends EventTarget {
 
@@ -8,23 +14,43 @@ export default class LogicHandler extends EventTarget {
 
     #logic = null;
 
-    constructor(storage, logic = true) {
+    #data = new Map();
+
+    constructor(storage, logic = true, events = EVENTS) {
         super();
         this.#storage = storage;
+        this.#compileLogic(logic, events);
+    }
+
+    #compileLogic(logic, events) {
         if (typeof logic == "object") {
-            /* LOGIC */
             this.#logic = LogicCompiler.compile(logic);
-            this.#value = !!this.#logic(ref => this.#storage.get(ref));
-            this.#storage.addEventListener("change", () => this.update());
+            this.#value = this.#execute();
+            if (events.length > 0) {
+                const storageEventManager = new EventTargetManager(this.#storage);
+                storageEventManager.set(events, () => {
+                    this.#update();
+                });
+            }
         } else if (logic != null) {
             this.#logic = logic;
             this.#value = !!logic;
         }
     }
 
-    update() {
+    #getValue(key) {
+        return this.#data.get(key) ?? this.#storage.get(key);
+    }
+
+    #execute() {
+        return !!this.#logic((key) => {
+            return this.#getValue(key);
+        });
+    }
+
+    #update = debounce(() => {
         if (typeof this.#logic == "function") {
-            const value = !!this.#logic(ref => this.#storage.get(ref));
+            const value = this.#execute();
             if (this.#value != value) {
                 this.#value = value;
                 const event = new Event("change");
@@ -32,6 +58,26 @@ export default class LogicHandler extends EventTarget {
                 this.dispatchEvent(event);
             }
         }
+    });
+
+    setDataValue(key, value) {
+        const old = this.#data.get(key);
+        if (old != value) {
+            this.#data.set(key, value);
+            this.#update();
+        }
+    }
+
+    removeDataValue(key) {
+        if (this.#data.has(key)) {
+            this.#data.delete(key);
+            this.#update();
+        }
+    }
+
+    clearData() {
+        this.#data.clear();
+        this.#update();
     }
 
     get value() {
