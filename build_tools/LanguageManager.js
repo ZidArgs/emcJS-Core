ae00b2f90ad7d01bf2b55d438ed5c70e444ed8b3
Denis Peters <denis.peters@udo.edu>
2022-05-19 09:30:51 +0200

    enable i18n to load language configurations; add LanguageManager build tool;

---
+++ build_tools/LanguageManager.js
@@ -0,0 +1,84 @@
+import fs from "fs";
+import path from "path";
+import through from "through";
+// import del from "del";
+
+const LNBR_SEQ = /(?:\r\n|\n|\r)/g
+const EXTRACTORS = [
+    {
+        regEx: /#\s+base:\s*(.+)\s*/,
+        func: (result, matches) => {
+            result["base"] = matches[1];
+        }
+    },
+    {
+        regEx: /#\s+language:\s*(.+)\s*/,
+        func: (result, matches) => {
+            result["label"] = matches[1];
+        }
+    },
+    {
+        regEx: /#\s+fragment:\s*(.+)\.([^.]+)\s*/,
+        func: (result, matches) => {
+            result["fragments"].push({
+                "type": matches[2],
+                "name": matches[1]
+            });
+        }
+    }
+];
+const FILES = new Map();
+
+function normalizePath(path) {
+    return path.replace(/\\/g, "/");
+}
+
+function analyzeFile(ref, file) {
+    const result = {
+        "label": ref,
+        "fragments": []
+    };
+    const fileContent = String(file.contents);
+    const lines = fileContent.split(LNBR_SEQ);
+    if (lines[0] == "# language file for track-oot") {
+        for (const line of lines) {
+            for (const {regEx, func} of EXTRACTORS) {
+                const matches = regEx.exec(line);
+                if (matches != null) {
+                    func(result, matches);
+                    break;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+class LanguageManager {
+
+    register(src = "/", dest = "/", sourcemaps = false) {
+        const files = [];
+        return through(function(file) {
+            const ref = path.basename(file.path, ".lang");
+            const result = analyzeFile(ref, file);
+            FILES.set(ref, result);
+            this.push(file);
+            return files.push(file);
+        }, function() {
+            return this.emit("end");
+        });
+    }
+
+    finish(dest = "/", metaFile = "_meta.json") {
+        const metaPath = path.resolve(dest, metaFile);
+        const metaPathNormal = normalizePath(metaPath);
+        console.log(`i18n meta file: ${metaPathNormal}`);
+        const files = Object.fromEntries(FILES.entries());
+        fs.writeFileSync(metaPath, JSON.stringify(files, null, 4));
+        FILES.clear();
+        return metaPath;
+    }
+
+}
+
+export default new LanguageManager();
--- src/util/I18n.js
+++ src/util/I18n.js
@@ -1,41 +1,120 @@
+import FileLoader from "./FileLoader.js";
+import ObjectHelper from "./helper/ObjectHelper.js";
+import Import from "./import/Import.js";
 import Logger from "./Logger.js";
 
-const LANGUAGES = new Map();
-const MISSING = new Map();
-let actLang = "";
+async function importFragment(basePath, type, name) {
+    switch (type) {
+        case "lang": {
+            const trans = await FileLoader.properties(`${basePath}/fragments/${name}.lang`);
+            return trans;
+        }
+        case "properties": {
+            const trans = await FileLoader.properties(`${basePath}/fragments/${name}.properties`);
+            return trans;
+        }
+        case "json": {
+            const trans = await FileLoader.jsonc(`${basePath}/fragments/${name}.json`);
+            return ObjectHelper.flatten(trans);
+        }
+        case "js": {
+            const [exec] = await Import.module(`${basePath}/fragments/${name}.js`);
+            const trans = exec();
+            return ObjectHelper.flatten(trans);
+        }
+        default: {
+            return {};
+        }
+    }
+}
 
 class I18n extends EventTarget {
 
-    setDefaultLanguage(lang) {
-        if (actLang != lang && LANGUAGES.has(lang)) {
-            actLang = lang;
-            const event = new Event("language");
-            event.language = lang;
-            this.dispatchEvent(event);
+    #languages = new Map();
+
+    #base = new Map();
+
+    #missing = new Map();
+
+    #default = "";
+
+    #active = "";
+
+    async loadTranslations(basePath = "/i18n") {
+        try {
+            const languages = await FileLoader.json(`${basePath}/_meta.json`);
+            const langLabels = {};
+            for (const key in languages) {
+                const data = languages[key];
+                langLabels[key] = data["label"];
+            }
+            for (const key in languages) {
+                const data = languages[key];
+                try {
+                    const translationFile = await FileLoader.properties(`${basePath}/${key}.lang`);
+                    let translation = {};
+                    for (const {type, name} of data["fragments"]) {
+                        const importTranslation = await importFragment(basePath, type, name);
+                        translation = Object.assign(translation, importTranslation);
+                    }
+                    translation = Object.assign(translation, translationFile);
+                    translation = Object.assign(translation, langLabels);
+                    this.setBase(key, data["base"]);
+                    this.setTranslation(key, translation);
+                } catch (err) {
+                    console.error(err);
+                    Logger.error(new Error(`could not load lang ${key}`), "I18n");
+                }
+            }
+        } catch (err) {
+            console.error(err);
+            Logger.error(new Error(`could not load language names`), "I18n");
         }
     }
 
-    setLanguage(lang) {
-        if (actLang != lang && LANGUAGES.has(lang)) {
-            actLang = lang;
-            const event = new Event("language");
-            event.language = lang;
-            this.dispatchEvent(event);
+    setBase(lang, base = "") {
+        if (typeof lang != "string" || !lang) {
+            return;
+        }
+        const oldBase = this.#base.get(lang) ?? "";
+        if (oldBase != base) {
+            if (base) {
+                this.#base.set(lang, base);
+            } else {
+                this.#base.delete(lang);
+            }
+            if (lang == this.#active) {
+                const changes = {};
+                const trans = this.#languages.get(lang);
+                const oldTrans = this.#languages.get(oldBase);
+                const newTrans = this.#languages.get(base);
+                const baseKeys = new Set(oldTrans.keys().concat(newTrans.keys()));
+                for (const key of baseKeys) {
+                    if (!trans.has(key)) {
+                        if (oldTrans.get(key) != newTrans.get(key)) {
+                            changes[key] = newTrans.get(key);
+                        }
+                    }
+                }
+                const event = new Event("translation");
+                event.changes = changes;
+                this.dispatchEvent(event);
+            }
         }
     }
 
     setTranslation(lang, values = {}) {
-        if (typeof lang != "string") {
+        if (typeof lang != "string" || !lang) {
             return;
         }
-        if (!LANGUAGES.has(lang)) {
-            LANGUAGES.set(lang, new Map());
+        if (!this.#languages.has(lang)) {
+            this.#languages.set(lang, new Map());
         }
-        if (!MISSING.has(lang)) {
-            MISSING.set(lang, new Set());
+        if (!this.#missing.has(lang)) {
+            this.#missing.set(lang, new Set());
         }
-        if (!actLang) {
-            actLang = lang;
+        if (!this.#active) {
+            this.#active = lang;
         }
         const changes = {};
         for (const key in values) {
@@ -46,57 +125,61 @@ class I18n extends EventTarget {
             if (!value || typeof value != "string") {
                 continue;
             }
-            LANGUAGES.get(lang).set(key, value);
-            changes[key] = value;
+            const trans = this.#languages.get(lang);
+            if (trans.get(key) != value) {
+                changes[key] = value;
+            }
+            trans.set(key, value);
         }
-        if (lang == actLang && Object.keys(changes).length) {
+        if (lang == this.#active && Object.keys(changes).length) {
             const event = new Event("translation");
             event.changes = changes;
             this.dispatchEvent(event);
         }
     }
 
-    getKeys(lang) {
-        const keys = new Set();
-        if (lang != null) {
-            const language = LANGUAGES.get(lang);
-            for (const [key] of language) {
-                keys.add(key);
-            }
-            const missing = MISSING.get(lang);
-            for (const key of missing) {
-                keys.add(key);
-            }
-        } else {
-            for (const [, language] of LANGUAGES) {
-                for (const [key] of language) {
-                    keys.add(key);
-                }
-            }
-            for (const [, missing] of MISSING) {
-                for (const key of missing) {
-                    keys.add(key);
-                }
+    set default(lang) {
+        if (typeof lang != "string") {
+            return;
+        }
+        if (this.#default != lang) {
+            const oldLang = this.language;
+            this.#default = lang;
+            const newLang = this.language;
+            if (oldLang != newLang) {
+                const event = new Event("language");
+                event.language = newLang;
+                this.dispatchEvent(event);
             }
         }
-        return Array.from(keys);
     }
 
-    getMissing(lang) {
-        const keys = new Set();
-        if (lang != null) {
-            const missing = MISSING.get(lang);
-            for (const key of missing) {
-                keys.add(key);
-            }
-        } else {
-            for (const [, missing] of MISSING) {
-                for (const key of missing) {
-                    keys.add(key);
-                }
+    set language(lang) {
+        if (typeof lang != "string") {
+            return;
+        }
+        if (this.#active != lang) {
+            const oldLang = this.language;
+            this.#active = lang;
+            const newLang = this.language;
+            if (oldLang != newLang) {
+                const event = new Event("language");
+                event.language = newLang;
+                this.dispatchEvent(event);
             }
         }
-        return Array.from(keys);
+    }
+
+    get language() {
+        const act = this.#active;
+        if (typeof act == "string" && act) {
+            return act;
+        }
+        const def = this.#default;
+        if (typeof def == "string" && def) {
+            return def;
+        }
+        return "";
     }
 
     /**
@@ -107,6 +190,10 @@ class I18n extends EventTarget {
     }
 
     get(key) {
+        return this.#getTranslation(this.language, key);
+    }
+
+    #getTranslation(lang, key) {
         if (key == null) {
             return "";
         }
@@ -114,19 +201,24 @@ class I18n extends EventTarget {
             return key;
         }
         if (typeof key == "string") {
-            if (actLang) {
+            if (typeof lang == "string" && lang) {
                 if (key == "") {
                     return "";
                 }
-                if (LANGUAGES.get(actLang).has(key)) {
-                    return LANGUAGES.get(actLang).get(key).trim();
+                if (this.#languages.get(lang).has(key)) {
+                    return this.#languages.get(lang).get(key).trim();
                 }
-                if (!MISSING.get(actLang).has(key)) {
-                    MISSING.get(actLang).add(key);
-                    Logger.warn(`translation for "${key}" missing`, "I18n");
-                    console.warn(`translation for "${key}" missing`);
+                if (this.#base.has(lang)) {
+                    const base = this.#base.has(lang)
+                    if (this.#languages.get(base).has(key)) {
+                        return this.#languages.get(base).get(key).trim();
+                    }
                 }
-                return key;
+                if (!this.#missing.get(lang).has(key)) {
+                    this.#missing.get(lang).add(key);
+                    Logger.warn(`translation for "${key}" in "${lang}" missing`, "I18n");
+                    console.warn(`translation for "${key}" in "${lang}" missing`);
+                }
             }
             return key;
         }
@@ -134,16 +226,74 @@ class I18n extends EventTarget {
     }
 
     has(key) {
+        return this.#hasTranslation(this.language, key);
+    }
+
+    #hasTranslation(lang, key) {
         if (typeof key != "string" || !key) {
             return false;
         }
-        if (actLang) {
-            if (LANGUAGES.get(actLang).has(key)) {
+        if (typeof lang == "string" && lang) {
+            if (this.#languages.get(lang).has(key)) {
                 return true;
             }
-            return false;
+            if (this.#base.has(lang)) {
+                const base = this.#base.has(lang)
+                if (this.#languages.get(base).has(key)) {
+                    return true;
+                }
+            }
         }
         return false;
+    }
+
+    // extras
+
+    getLanguages() {
+        return this.#languages.keys();
+    }
+
+    getKeys(lang) {
+        const keys = new Set();
+        if (typeof lang == "string" && lang) {
+            const language = this.#languages.get(lang);
+            for (const [key] of language) {
+                keys.add(key);
+            }
+            const missing = this.#missing.get(lang);
+            for (const key of missing) {
+                keys.add(key);
+            }
+        } else {
+            for (const [, language] of this.#languages) {
+                for (const [key] of language) {
+                    keys.add(key);
+                }
+            }
+            for (const [, missing] of this.#missing) {
+                for (const key of missing) {
+                    keys.add(key);
+                }
+            }
+        }
+        return Array.from(keys);
+    }
+
+    getMissing(lang) {
+        const keys = new Set();
+        if (typeof lang == "string" && lang) {
+            const missing = this.#missing.get(lang);
+            for (const key of missing) {
+                keys.add(key);
+            }
+        } else {
+            for (const [, missing] of this.#missing) {
+                for (const key of missing) {
+                    keys.add(key);
+                }
+            }
+        }
+        return Array.from(keys);
     }
 
 }
