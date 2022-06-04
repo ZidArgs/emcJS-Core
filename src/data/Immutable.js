06ff467c70e6a84137d57d4c0b22067bf015e182
Denis Peters <denis.peters@udo.edu>
2022-06-04 14:52:25 +0200

    add immutable check to prevent reimmute;

--- src/data/Immutable.js
+++ src/data/Immutable.js
@@ -16,23 +16,29 @@ const HANDLER = {
     }
 };
 
+const TYPE_TAG = Symbol("Immutable");
+
 /**
  * Create an immutable Object utilizing Proxy
  * @param {Object} data an object to immute, primitives can not be immuted
  * @returns {Proxy} the Proxy immuting the data
  */
 export function immute(data) {
-    if (typeof data == "object") {
+    if (typeof data == "object" && !data[TYPE_TAG]) {
         if (Array.isArray(data)) {
             const res = data.map(immute);
-            return new Proxy(res, HANDLER);
+            const proxy = new Proxy(res, HANDLER);
+            Object.defineProperty(res, TYPE_TAG, {value: true});
+            return proxy;
         } else {
             const res = {};
             for (const key in data) {
                 const value = data[key];
                 res[key] = immute(value);
             }
-            return new Proxy(res, HANDLER);
+            const proxy = new Proxy(res, HANDLER);
+            Object.defineProperty(res, TYPE_TAG, {value: true});
+            return proxy;
         }
     }
     return data;
