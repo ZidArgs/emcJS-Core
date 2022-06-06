cd7b47d0836fdf2e8079bf15c46042d5d04eeaf6
Denis Peters <denis.peters@udo.edu>
2022-06-06 02:09:44 +0200

    add debounceCacheData;

--- src/util/Debouncer.js
+++ src/util/Debouncer.js
@@ -17,3 +17,26 @@ export function debounce(func, wait = 0) {
         }, wait);
     };
 }
+
+export function debounceCacheData(func, wait = 0) {
+    if (typeof func != "function") {
+        throw new TypeError(`func parameter must be of type "function" but was "${typeof ref}"`);
+    }
+    if (typeof wait != "number") {
+        throw new TypeError(`wait parameter must be of type "number" but was "${typeof ref}"`);
+    }
+    if (isNaN(wait) || wait < 0) {
+        wait = 0;
+    }
+    let timeout;
+    let cache = [];
+    return function(data) {
+        cache.push(data);
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.call(context, cache);
+            cache = [];
+        }, wait);
+    };
+}
