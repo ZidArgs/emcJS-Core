213b2cbdd8240333ea3da161ffb1aa7539ebd6d6
Denis Peters <denis.peters@udo.edu>
2022-06-19 19:22:23 +0200

    change default panel min-sizes;

--- src/ui/layout/Panel.js
+++ src/ui/layout/Panel.js
@@ -1,4 +1,6 @@
 import CustomElement from "../element/CustomElement.js";
+// import TPL from "./Panel.js.html" assert {type: "html"};
+import STYLE from "./Panel.js.css" assert {type: "css"};
 
 const REG = new Map();
 
@@ -6,6 +8,8 @@ export default class Panel extends CustomElement {
 
     constructor() {
         super();
+        // this.shadowRoot.append(TPL.generate());
+        STYLE.apply(this.shadowRoot);
         /* --- */
         if (new.target === Panel) {
             throw new Error("can not construct abstract class");
