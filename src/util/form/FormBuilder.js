b7f01b5d4e2c7327cbc3d49174a59ef7ed2000c3
Denis Peters <zidargs@gmail.com>
2023-01-11 01:41:07 +0100

    extend FormBuilder concept;

--- src/util/form/FormBuilder.js
+++ src/util/form/FormBuilder.js
@@ -4,47 +4,65 @@ export default class FormBuilder {
     build(window, options) {
         for (const ref in options) {
             const option = options[ref];
-            const category = option.category;
-            const label = option.label || ref;
-            const desc = option.desc;
             switch (option.type) {
-                case "string": {
+                case "action-button": {
                     // existing
                 } break;
-                case "number": {
+                case "link-button": {
                     // existing
                 } break;
-                case "range": {
-                    const min = parseFloat(option.min);
-                    const max = parseFloat(option.max);
-                    window.addRangeInput(category, ref, label, desc, option.default, option.visible, option.resettable, min, max);
-                } break;
-                case "check": {
-                    window.addCheckInput(category, ref, label, desc, option.default, option.visible, option.resettable);
-                } break;
-                case "color": {
+                case "fieldset": {
                     // existing
                 } break;
-                case "password": {
-                    // existing - new
-                } break;
-                case "hotkey": {
-                    window.addKeyInput(category, ref, label, desc, option.default, option.visible, option.resettable);
-                } break;
-                case "choice": {
-                    window.addChoiceInput(category, ref, label, desc, option.default, option.visible, option.resettable, option.values);
-                } break;
-                case "list": {
-                    window.addListSelectInput(category, ref, label, desc, option.default, option.visible, option.resettable, option.multiple, option.values);
-                } break;
-                case "button": {
-                    window.addButton(category, ref, label, desc, option.visible, option.text, () => {
-                        const event = new Event(option.event ?? "button");
-                        event.action = option.action;
-                        window.dispatchEvent(event);
-                    });
+                case "input": {
+                    this.#buildInput(window, option, ref)
                 } break;
             }
+        }
+    }
+
+    #buildInput(window, option, ref) {
+        const category = option.category; // deprecated
+        const label = option.label || ref;
+        const desc = option.desc;
+        switch (option.is) {
+            case "string": {
+                // existing
+            } break;
+            case "number": {
+                // existing
+                // missing: min, max
+            } break;
+            case "range": {
+                const min = parseFloat(option.min);
+                const max = parseFloat(option.max);
+                window.addRangeInput(category, ref, label, desc, option.default, option.visible, option.resettable, min, max);
+            } break;
+            case "check": {
+                window.addCheckInput(category, ref, label, desc, option.default, option.visible, option.resettable);
+            } break;
+            case "color": {
+                // existing
+            } break;
+            case "password": {
+                // existing - new
+            } break;
+            case "hotkey": {
+                window.addKeyInput(category, ref, label, desc, option.default, option.visible, option.resettable);
+            } break;
+            case "choice": {
+                window.addChoiceInput(category, ref, label, desc, option.default, option.visible, option.resettable, option.values);
+            } break;
+            case "list": {
+                window.addListSelectInput(category, ref, label, desc, option.default, option.visible, option.resettable, option.multiple, option.values);
+            } break;
+            case "button": {
+                window.addButton(category, ref, label, desc, option.visible, option.text, () => {
+                    const event = new Event(option.event ?? "button");
+                    event.action = option.action;
+                    window.dispatchEvent(event);
+                });
+            } break;
         }
     }
 
