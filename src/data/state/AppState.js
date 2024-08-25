bb146239f194101772c268d9e3b6911a9d1f3bf3
Denis Weiß <zidargs@gmail.com>
2024-08-25 18:57:31 +0200

    add overwriteClean; split getAllFromCategories of getAll; add regex pattern for category names; add missing docs;

--- src/data/state/AppState.js
+++ src/data/state/AppState.js
@@ -1,5 +1,12 @@
 import ObservableStorage from "../storage/observable/ObservableStorage.js";
 
+const CATEGORY_NAME_REGEX = /^[A-Za-z][A-Za-z0-9_]*$/;
+
+/**
+ * Holds multiple storages based on a category system.
+ *
+ * Initially storages default to {@link ObservableStorage} but through registering any Storage extending {@link ObservableStorage} can be used.
+ */
 export default class AppState extends EventTarget {
 
     #storageChangeHandler = new Map();
@@ -12,6 +19,11 @@ export default class AppState extends EventTarget {
 
     #requestedStorages = new Map();
 
+    /**
+     * Create a new {@link AppState} and optionally set its initial data.
+     *
+     * @param {Object.<string, Object.<string, *>>} [data] an Object containing the initial data
+     */
     constructor(data) {
         super();
         /* --- */
@@ -25,6 +37,11 @@ export default class AppState extends EventTarget {
         }
     }
 
+    /**
+     * Create a new instance of {@link AppState} and copy all the {@link ObservableStorage}s and data to it.
+     *
+     * @returns {AppState} the new AppState with all the Storages and data cloned
+     */
     clone() {
         const instance = new AppState();
         for (const [key, value] of this.#meta) {
@@ -49,6 +66,9 @@ export default class AppState extends EventTarget {
         return instance;
     }
 
+    /**
+     * Clears all data from the internal {@link ObservableStorage}s and the meta storage.
+     */
     purge() {
         this.#meta.clear();
         for (const [, dataStorage] of this.#registeredStorages) {
@@ -59,7 +79,11 @@ export default class AppState extends EventTarget {
         }
     }
 
-    /* SERIALIZATION */
+    /**
+     * Returns an object representation of the data of all internal {@link ObservableStorage}s and the meta storage.
+     *
+     * @returns {{meta: Object.<string, *>, data: Object.<string, Object.<string, *>>}} an Object containing data of all internal Storages and the meta storage
+     */
     serialize() {
         const res = {
             meta: {},
@@ -77,6 +101,11 @@ export default class AppState extends EventTarget {
         return res;
     }
 
+    /**
+     * Clears and fills the internal {@link ObservableStorage}s and the meta storage with the given data, handling it as a stateload.
+     *
+     * @param {{meta: Object.<string, *>, data: Object.<string, Object.<string, *>>}} obj an Object containing data for the internal Storages and the meta storage
+     */
     deserialize(obj = {}) {
         const {
             data = {},
@@ -106,6 +135,12 @@ export default class AppState extends EventTarget {
         this.dispatchEvent(ev);
     }
 
+    /**
+     * Overwrites the internal {@link ObservableStorage}s with the given data, handling it as a stateload.
+     * Only overwrites the given keys for each respective Storage.
+     *
+     * @param {Object.<string, Object.<string, *>>} data an Object containing data for the internal Storages
+     */
     overwrite(data = {}) {
         for (const category in data) {
             const dataStorage = this.getStorage(category);
@@ -120,6 +155,34 @@ export default class AppState extends EventTarget {
         }
     }
 
+    /**
+     * Overwrites the internal {@link ObservableStorage}s with the given data, handling it as a stateload.
+     * Clears the respective Storage before writing the data.
+     *
+     * @param {Object.<string, Object.<string, *>>} data an Object containing data for the internal Storages
+     */
+    overwriteClean(data = {}) {
+        for (const category in data) {
+            const dataStorage = this.getStorage(category);
+            if (dataStorage != null) {
+                const buffer = data[category];
+                if (buffer == null) {
+                    dataStorage.clear();
+                } else {
+                    dataStorage.deserialize(data[category]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns an object representation of the default data of the internal {@link ObservableStorage}s and the meta storage the AppState holds.
+     *
+     * Additionally the initial data for the internal {@link ObservableStorage}s can be set.
+     *
+     * @param {Object.<string, Object.<string, *>>} [initialData] an Object containing the initial data
+     * @returns {{meta: Object.<string, *>, data: Object.<string, Object.<string, *>>}} an Object containing the default data of the internal Storages and the meta storage
+     */
     createNewData(initialData = {}) {
         const res = {
             meta: {},
@@ -144,11 +207,12 @@ export default class AppState extends EventTarget {
         return res;
     }
 
-    /* META */
-    getMeta(key) {
-        return this.#meta.get(key);
-    }
-
+    /**
+     * Writes data to the meta storage.
+     *
+     * @param {string} key the key to store the data
+     * @param {*} value the data to store
+     */
     setMeta(key, value) {
         const oldValue = this.#meta.get(key);
         if (oldValue != value) {
@@ -159,17 +223,37 @@ export default class AppState extends EventTarget {
         }
     }
 
-    /* STORAGES */
+    /**
+     * Reads data from the meta storage.
+     *
+     * @param {string} key the key of the data to read
+     * @returns {*} the stored data or `undefined`
+     */
+    getMeta(key) {
+        return this.#meta.get(key);
+    }
+
+    /**
+     * Register an {@link ObservableStorage} or an extended class of it to a specified category if no Storage was previously registered.
+     * This can be used to register custom Storages as the default is always {@link ObservableStorage}.
+     *
+     * If a default Storage has already been used, the registration will try to migrate all the data from the default Storage to the
+     * newly registered one by serialization/deserialization.
+     *
+     * @param {string} category the category to register the storage to (matches [A-Za-z][A-Za-z0-9_]*)
+     * @param {ObservableStorage} dataStorage the Storage implementation to register
+     * @param {string} eventName the event name to listen to for detecting cchanges (defaults to `change`)
+     */
     registerStorage(category, dataStorage, eventName = "change") {
         const storageCategory = category.toString();
+        if (!CATEGORY_NAME_REGEX.test(storageCategory)) {
+            throw new Error(`failed to retrieve storage "${storageCategory}" - category does not match pattern [A-Za-z][A-Za-z0-9_]*`);
+        }
         if (!(dataStorage instanceof ObservableStorage)) {
             throw new TypeError("unknown storage implementation, expected DataStorage");
         }
         if (this.#registeredStorages.has(storageCategory)) {
-            throw new Error(`special storage with name "${storageCategory}" already registerred`);
-        }
-        if (storageCategory.includes(".")) {
-            throw new Error(`failed to register storage "${storageCategory}" - category must not include "."`);
+            throw new Error(`storage with name "${storageCategory}" already registerred`);
         }
         const handler = this.#getChangeHandler(storageCategory);
         if (this.#requestedStorages.has(storageCategory)) {
@@ -186,10 +270,18 @@ export default class AppState extends EventTarget {
         this.dispatchEvent(ev);
     }
 
+    /**
+     * Returns a {@link ObservableStorage} or an extended class of it registered to a specified category.
+     *
+     * If no {@link ObservableStorage} has been registered, it stores and returns a default Storage.
+     *
+     * @param {string} category the category for which to get the storage for (matches [A-Za-z][A-Za-z0-9_]* or empty string)
+     * @returns {ObservableStorage} the registered Storage or an default Storage
+     */
     getStorage(category) {
         const storageCategory = category.toString();
-        if (storageCategory.includes(".")) {
-            throw new Error(`failed to retrieve storage "${storageCategory}" - category must not include "."`);
+        if (!CATEGORY_NAME_REGEX.test(storageCategory) && storageCategory !== "") {
+            throw new Error(`failed to retrieve storage "${storageCategory}" - category does not match pattern [A-Za-z][A-Za-z0-9_]* or empty string`);
         }
         if (this.#registeredStorages.has(storageCategory)) {
             return this.#registeredStorages.get(storageCategory);
@@ -219,7 +311,13 @@ export default class AppState extends EventTarget {
         return handler;
     }
 
-    /* DATA */
+    /**
+     * Write data to the {@link ObservableStorage} represented by its category.
+     *
+     * @param {string} category the category of the Storage to write the data to
+     * @param {string} key the key to store the data
+     * @param {*} value the data to store
+     */
     set(category, key, value) {
         const dataStorage = this.getStorage(category);
         if (dataStorage != null) {
@@ -231,6 +329,14 @@ export default class AppState extends EventTarget {
         }
     }
 
+    /**
+     * Reads data from the {@link ObservableStorage} represented by its category.
+     *
+     * @param {string} category the category of the Storage to read the data from
+     * @param {string} key the key of the data to read
+     * @param {*} [def] the default value if no value is present (defaults to `undefined`)
+     * @returns {*} the stored data or the default value
+     */
     get(category, key, def) {
         const dataStorage = this.getStorage(category);
         if (dataStorage != null) {
@@ -241,34 +347,45 @@ export default class AppState extends EventTarget {
         return def;
     }
 
-    getAll(category) {
-        if (category == null) {
-            const res = {};
-            for (const [cat, dataStorage] of this.#registeredStorages) {
-                res[cat] = dataStorage.getAll();
-            }
-            for (const [cat, dataStorage] of this.#requestedStorages) {
-                res[cat] = dataStorage.getAll();
-            }
-            return res;
-        } else if (Array.isArray(category)) {
-            const res = {};
-            for (const cat of category) {
-                const dataStorage = this.getStorage(cat);
-                if (dataStorage != null) {
-                    res[cat] = dataStorage.getAll();
-                }
-            }
-            return res;
-        } else {
-            const dataStorage = this.getStorage(category);
-            if (dataStorage != null) {
-                return dataStorage.getAll();
-            }
-            return {};
+    /**
+     * Returns an object representation of the data of all internal {@link ObservableStorage}s.
+     *
+     * @returns {Object.<string, Object.<string, *>>} an Object containing data of all internal Storages
+     */
+    getAll() {
+        const res = {};
+        for (const [category, dataStorage] of this.#registeredStorages) {
+            res[category] = dataStorage.getAll();
         }
+        for (const [category, dataStorage] of this.#requestedStorages) {
+            res[category] = dataStorage.getAll();
+        }
+        return res;
     }
 
+    /**
+     * Returns an object representation of the data of the specified internal {@link ObservableStorage}s.
+     *
+     * @param {string} categories the categories of the Storages to read the data from
+     * @returns {Object.<string, Object.<string, *>>} an Object containing data of the specified internal Storages
+     */
+    getAllFromCategories(...categories) {
+        const res = {};
+        for (const category of categories) {
+            const dataStorage = this.getStorage(category);
+            if (dataStorage != null) {
+                res[category] = dataStorage.getAll();
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Delete data from the {@link ObservableStorage} represented by its category.
+     *
+     * @param {string} category the category of the Storage to delete the data from
+     * @param {string} key the key of the data to delete
+     */
     delete(category, key) {
         const dataStorage = this.getStorage(category);
         if (dataStorage != null && dataStorage.has(key)) {
