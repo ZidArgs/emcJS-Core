e788be726b646191dc78c64bcd29c327d0f1e6eb
Denis Weiß <zidargs@gmail.com>
2025-02-16 00:16:58 +0100

    change CodeInput to use textarea as base input;

--- src/ui/form/element/input/code/CodeInput.js
+++ src/ui/form/element/input/code/CodeInput.js
@@ -5,12 +5,17 @@ import {
 import {
     registerFocusable
 } from "../../../../../util/helper/html/getFocusableElements.js";
+import {
+    safeSetAttribute
+} from "../../../../../util/helper/ui/NodeAttributes.js";
 import FormElementRegistry from "../../../../../data/registry/form/FormElementRegistry.js";
 import TPL from "./CodeInput.js.html" assert {type: "html"};
 import STYLE from "./CodeInput.js.css" assert {type: "css"};
 
-// XXX currently only as readonly usable
-// FIXME ctrl+z/y
+const LAST_CHARACTER_NEWLINE = /\n$/;
+
+// TODO add css variables for code editor colors
+// TODO customize scrollbars to have sharp corners and transparent backgrounds
 export default class CodeInput extends AbstractFormElement {
 
     #fieldEl;
@@ -19,13 +24,21 @@ export default class CodeInput extends AbstractFormElement {
 
     #lineNumbersEl;
 
+    #lineMarkerEl;
+
     #inputEl;
+
+    #inputMirrorEl;
+
+    #outputWrapperEl;
 
     #lineInfoEl;
 
     #expandButtonEl;
 
-    #value = "";
+    #lineCount = 1;
+
+    #currentLine = 0;
 
     constructor() {
         super();
@@ -35,23 +48,32 @@ export default class CodeInput extends AbstractFormElement {
         /* --- */
         this.#containerEl = this.shadowRoot.getElementById("container");
         this.#lineNumbersEl = this.shadowRoot.getElementById("line-numbers");
+        this.#lineMarkerEl = this.shadowRoot.getElementById("line-marker");
         this.#lineInfoEl = this.shadowRoot.getElementById("line-info");
         this.#expandButtonEl = this.shadowRoot.getElementById("expand-button");
+        this.#outputWrapperEl = this.shadowRoot.getElementById("output-wrapper");
         this.#inputEl = this.shadowRoot.getElementById("input");
+        this.#inputMirrorEl = this.shadowRoot.getElementById("input-mirror");
         this.#inputEl.addEventListener("input", () => {
-            const children = [...this.#inputEl.children];
-            const lines = children.length;
-            this.#containerEl.style.setProperty("--num-digits", `${lines}`.length);
-            this.#lineNumbersEl.innerHTML = "";
-            for (let i = 0; i < lines; ++i) {
-                const numberEl = document.createElement("div");
-                numberEl.innerText = i + 1;
-                this.#lineNumbersEl.append(numberEl);
-            }
-            this.#onInput(children.map((ch) => ch.innerText).join("\n"));
+            const value = this.#inputEl.value;
+            this.#updateText(value);
+            this.#onInput(value);
         }, {passive: true});
         this.#inputEl.addEventListener("keydown", (event) => {
-            if (event.key === "Backspace" && this.#inputEl.children.length <= 1 && this.#inputEl.children[0]?.innerText.trim() === "") {
+            if (event.key === "Tab") {
+                const value = this.#inputEl.value;
+                const before_tab = value.slice(0, this.#inputEl.selectionStart);
+                const after_tab = value.slice(this.#inputEl.selectionEnd, value.length);
+                const cursor_pos = this.#inputEl.selectionEnd + 1;
+
+                const newValue = before_tab + "\t" + after_tab;
+                this.#inputEl.value = newValue;
+                this.#inputEl.selectionStart = cursor_pos;
+                this.#inputEl.selectionEnd = cursor_pos;
+
+                this.#updateText(newValue);
+                this.#onInput(newValue);
+
                 event.preventDefault();
                 return false;
             }
@@ -61,66 +83,32 @@ export default class CodeInput extends AbstractFormElement {
             }
         });
         /* --- */
-        this.#inputEl.addEventListener("paste", (event) => {
-            event.preventDefault();
-            const text = event.clipboardData.getData("text/plain");
-
-            const selection = this.shadowRoot.getSelection();
-            if (!selection.rangeCount) {
-                return;
-            }
-            const range = selection.getRangeAt(0);
-
-            let startContainer = range.startContainer;
-            if (startContainer.nodeType === 3) {
-                startContainer = startContainer.parentElement;
-            }
-            let endContainer = range.endContainer;
-            if (endContainer.nodeType === 3) {
-                endContainer = endContainer.parentElement;
-            }
-
-            const [resultText, selectionOffset] = this.#modifyText(startContainer, range.startOffset, endContainer, range.endOffset, text);
-
-            let textNode = endContainer.childNodes[0];
-            if (textNode == null) {
-                textNode = document.createTextNode("");
-                endContainer.append(textNode);
-                selection.setPosition(textNode, 0);
-            } else {
-                selection.setPosition(textNode, selectionOffset);
-            }
-
-            this.#value = resultText;
-            this.value = resultText;
-        });
-        /* --- */
         this.#containerEl.addEventListener("scroll", () => {
-            this.#inputEl.scrollTop = this.#containerEl.scrollTop;
+            const scrollTop = this.#containerEl.scrollTop;
+            this.#inputEl.scrollTop = scrollTop;
+            this.#outputWrapperEl.scrollTop = scrollTop;
+            this.#updateCaretPosition(true);
         }, {passive: true});
         this.#inputEl.addEventListener("scroll", () => {
-            this.#containerEl.scrollTop = this.#inputEl.scrollTop;
+            const scrollTop = this.#inputEl.scrollTop;
+            this.#containerEl.scrollTop = scrollTop;
+            this.#outputWrapperEl.scrollTop = scrollTop;
+            this.#updateCaretPosition(true);
         }, {passive: true});
         /* --- */
         this.#inputEl.addEventListener("focus", (event) => {
             event.stopPropagation();
             const selection = this.shadowRoot.getSelection();
-            this.#styleNode(selection);
+            if (selection.focusNode != null) {
+                this.#updateCaretPosition();
+            }
         });
         document.addEventListener("selectionchange", () => {
             const selection = this.shadowRoot.getSelection();
-            this.#styleNode(selection);
+            if (selection.focusNode != null) {
+                this.#updateCaretPosition();
+            }
         }, {passive: true});
-        this.#inputEl.addEventListener("blur", () => {
-            const oldNodes = this.#inputEl.querySelectorAll(".caret");
-            const oldNumbersNodes = this.#lineNumbersEl.querySelectorAll(".caret");
-            for (const oldNode of oldNodes) {
-                oldNode.classList.remove("caret");
-            }
-            for (const oldNode of oldNumbersNodes) {
-                oldNode.classList.remove("caret");
-            }
-        });
         /* --- */
         this.#expandButtonEl.addEventListener("click", () => {
             if (this.#fieldEl.classList.contains("expanded")) {
@@ -134,7 +122,6 @@ export default class CodeInput extends AbstractFormElement {
     }
 
     #onInput = debounce((value) => {
-        this.#value = value;
         this.value = value;
     }, 300);
 
@@ -156,151 +143,71 @@ export default class CodeInput extends AbstractFormElement {
         switch (name) {
             case "readonly": {
                 if (oldValue != newValue) {
-                    if (this.readonly) {
-                        this.#inputEl.removeAttribute("contenteditable");
-                    } else {
-                        this.#inputEl.setAttribute("contenteditable", "");
-                    }
+                    safeSetAttribute(this.#inputEl, "readonly", this.readonly);
                 }
             } break;
         }
     }
 
     renderValue(value) {
-        if (value != null && value !== "") {
-            if (value !== this.#value) {
-                this.#inputEl.innerHTML = "";
-                const lines = value.split(/\n/);
-                let lastEl = null;
-                for (const line of lines) {
-                    lastEl = document.createElement("div");
-                    lastEl.innerText = line;
-                    this.#inputEl.appendChild(lastEl);
-                }
-                /* --- */
-                const selection = this.shadowRoot.getSelection();
-                selection.collapse(lastEl, 1);
-                /* --- */
-                this.#printLineNumbers(lines.length);
-            }
+        this.#inputEl.value = value ?? "";
+    }
+
+    #updateText(value) {
+        const lineCount = value.split("\n").length;
+        this.#printLineNumbers(lineCount);
+        if (LAST_CHARACTER_NEWLINE.test(value)) {
+            this.#inputMirrorEl.innerText = `${value} `;
         } else {
-            this.#inputEl.innerHTML = "<div><br /></div>";
-            this.#lineNumbersEl.innerHTML = "<div>1</div>";
-            this.#containerEl.style.setProperty("--num-digits", 1);
+            this.#inputMirrorEl.innerText = value;
         }
     }
 
-    #styleNode = debounce((selection) => {
-        const oldNodes = this.#inputEl.querySelectorAll(".caret");
-        const oldNumbersNodes = this.#lineNumbersEl.querySelectorAll(".caret");
-        for (const oldNode of oldNodes) {
-            oldNode.classList.remove("caret");
-        }
-        for (const oldNode of oldNumbersNodes) {
-            oldNode.classList.remove("caret");
+    #updateCaretPosition = debounce((force = false) => {
+        const selectionStart = this.#inputEl.selectionStart;
+        const selectionEnd = this.#inputEl.selectionEnd;
+        const selectionForward = this.#inputEl.selectionDirection === "forward";
+        const value = this.#inputEl.value;
+        const textBeforeCursor = value.slice(0, selectionForward ? selectionEnd : selectionStart);
+        const lines = textBeforeCursor.split("\n");
+        const currentLine = lines.length - 1;
+        const focusOffset = lines.at(-1)?.length ?? 0;
+
+        if (this.#currentLine != currentLine || force) {
+            this.#currentLine = currentLine;
+
+            const oldNumbersNodes = this.#lineNumbersEl.querySelectorAll(".caret");
+            for (const oldNode of oldNumbersNodes) {
+                oldNode.classList.remove("caret");
+            }
+
+            const numberEl = this.#lineNumbersEl.children[currentLine];
+            if (numberEl != null) {
+                numberEl.classList.add("caret");
+            }
+
+            this.#lineMarkerEl.style.top = `calc(${1.4 * currentLine}em + 5px)`;
         }
 
-        let focusNode = selection.focusNode;
-        if (focusNode != null) {
-            if (focusNode.nodeType === 3) {
-                focusNode = focusNode.parentElement;
-            }
-            if (focusNode instanceof HTMLElement) {
-                focusNode.classList.add("caret");
-                const index = this.#getChildElementIndex(focusNode);
-                const numberEl = this.#lineNumbersEl.children[index];
-                if (numberEl != null) {
-                    numberEl.classList.add("caret");
-                }
-
-                const selectedString = selection.toString();
-                const length = selectedString.length;
-                if (length > 0) {
-                    this.#lineInfoEl.innerText = `Ln ${index + 1}, Col ${selection.focusOffset + 1} (${length} selected)`;
-                } else {
-                    this.#lineInfoEl.innerText = `Ln ${index + 1}, Col ${selection.focusOffset + 1}`;
-                }
-            }
+        const length = Math.abs(selectionEnd - selectionStart);
+        if (length > 0) {
+            this.#lineInfoEl.innerText = `Ln ${currentLine + 1}, Col ${focusOffset + 1} (${length} selected)`;
         } else {
-            this.#lineInfoEl.innerText = "Ln 1, Col 1";
+            this.#lineInfoEl.innerText = `Ln ${currentLine + 1}, Col ${focusOffset + 1}`;
         }
     });
 
-    #getChildElementIndex(node) {
-        if (node.parentNode != null) {
-            return Array.prototype.indexOf.call(node.parentNode.children, node);
-        }
-        return -1;
-    }
-
-    #modifyText(startContainer, startOffset, endContainer, endOffset, text) {
-        const textLines = text.split("\n");
-        const resultLines = [];
-        let lastLineLength = 0;
-        const startLine = startContainer.innerText;
-        const startLinePart = startLine.slice(0, startOffset);
-        const endLine = endContainer.innerText;
-        const endLinePart = endLine.slice(endOffset);
-
-        let currentChild = this.#inputEl.children[0];
-        while (currentChild !== startContainer) {
-            resultLines.push(currentChild.innerText);
-            currentChild = currentChild.nextSibling;
-        }
-
-        while (startContainer !== endContainer) {
-            const nextContainer = startContainer.nextSibling;
-            startContainer.remove();
-            startContainer = nextContainer;
-        }
-
-        if (textLines.length > 1) {
-            const firstLine = startLinePart + textLines.shift();
-            resultLines.push(firstLine);
-            const firstContainer = document.createElement("div");
-            firstContainer.innerText = firstLine;
-            endContainer.insertAdjacentElement("beforebegin", firstContainer);
-
-            while (textLines.length > 1) {
-                const currentLine = textLines.shift();
-                resultLines.push(currentLine);
-                const currentContainer = document.createElement("div");
-                currentContainer.innerText = currentLine;
-                endContainer.insertAdjacentElement("beforebegin", currentContainer);
-            }
-
-            const lastTextLine = textLines.shift();
-            lastLineLength = lastTextLine.length;
-            const lastLine = lastTextLine + endLinePart;
-            resultLines.push(lastLine);
-            endContainer.innerText = lastLine;
-        } else {
-            const lastTextLine = textLines.shift();
-            lastLineLength = startLinePart.length + lastTextLine.length;
-            const line = startLinePart + lastTextLine + endLinePart;
-            resultLines.push(line);
-            endContainer.innerText = line;
-        }
-
-        currentChild = endContainer.nextSibling;
-        while (currentChild != null) {
-            resultLines.push(currentChild.innerText);
-            currentChild = currentChild.nextSibling;
-        }
-
-        this.#printLineNumbers(resultLines.length);
-
-        return [resultLines.join("\n"), lastLineLength];
-    }
-
     #printLineNumbers(lineCount) {
-        this.#lineNumbersEl.innerHTML = "";
-        for (let i = 0; i < lineCount; ++i) {
-            const numberEl = document.createElement("div");
-            numberEl.innerText = i + 1;
-            this.#lineNumbersEl.append(numberEl);
+        if (this.#lineCount != lineCount) {
+            this.#lineCount = lineCount;
+            this.#lineNumbersEl.innerHTML = "";
+            for (let i = 0; i < lineCount; ++i) {
+                const numberEl = document.createElement("div");
+                numberEl.innerText = i + 1;
+                this.#lineNumbersEl.append(numberEl);
+            }
+            this.#containerEl.style.setProperty("--num-digits", `${lineCount}`.length);
         }
-        this.#containerEl.style.setProperty("--num-digits", `${lineCount}`.length);
     }
 
 }
