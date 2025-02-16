9be5408a209a09a33ccc21ae4ea1b241e717b6fe
Denis Weiß <zidargs@gmail.com>
2025-02-17 00:48:41 +0100

    fix CodeInput to render programmatically set value;

--- src/ui/form/element/input/code/CodeInput.js
+++ src/ui/form/element/input/code/CodeInput.js
@@ -151,6 +151,7 @@ export default class CodeInput extends AbstractFormElement {
 
     renderValue(value) {
         this.#inputEl.value = value ?? "";
+        this.#updateText(value);
     }
 
     #updateText(value) {
