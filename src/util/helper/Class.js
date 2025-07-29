5fba3eb2f68ec6590057540820b494e4320babe1
Denis Weiß <zidargs@gmail.com>
2025-07-30 00:15:32 +0200

    add classExtends to Class helper;

--- src/util/helper/Class.js
+++ src/util/helper/Class.js
@@ -22,18 +22,25 @@ export function allInstanceOf(clazz = Object, ...objList) {
     return true;
 }
 
-export function isClass(obj) {
-    if (obj == null) {
+export function isClass(clazz = Object) {
+    if (clazz == null) {
         return false;
     }
-    const isClass = isClassDefinition(obj);
-    if (obj.prototype == null) {
+    const isClass = isClassDefinition(clazz);
+    if (clazz.prototype == null) {
         return isClass;
     }
-    const isProtoClass = isClassDefinition(obj.prototype);
+    const isProtoClass = isClassDefinition(clazz.prototype);
     return isClass || isProtoClass;
 }
 
-function isClassDefinition(obj) {
-    return obj.constructor?.toString().slice(0, 5) === "class";
+function isClassDefinition(clazz) {
+    return clazz.constructor?.toString().slice(0, 5) === "class";
+}
+
+export function classExtends(clazzA = Object, clazzB = Object) {
+    if (!isClass(clazzA) || !isClass(clazzB)) {
+        return false;
+    }
+    return clazzA.prototype instanceof clazzB;
 }
